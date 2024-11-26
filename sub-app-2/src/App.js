104d9cb0152d41c22f4f1b48832ea45141c185d6
NazliQ <nazlipikulser@gmail.com>
2024-11-26 15:52:53 +0100

    Updatet main app for routing and maed placeholder components

--- sub-app-2/src/App.js
+++ sub-app-2/src/App.js
@@ -1,14 +1,38 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Index from './Home/Index';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavMenu from './shared/Navbar';
 
 function App() {
   return (
-    <div className='App'>
+    <Router>
+    <div className='App d-flex'>
       <NavMenu />
-      <Index />
-    </div>
+      <main className="flex-grow-1 p-3">
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/my" element={<MyProducts />} />
+            <Route path="/products/new" element={<CreateProduct />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+        </div>
+        </Router>
   );
 }
+
+
+// Temporary placeholder components
+const Products = () => <h1>Products Page</h1>;
+const MyProducts = () => <h1>My Products</h1>;
+const CreateProduct = () => <h1>Create Product</h1>;
+const Account = () => <h1>Account Page</h1>;
+const Privacy = () => <h1>Privacy Page</h1>;
+const Login = () => <h1>Login Page</h1>;
+
 
 export default App;
