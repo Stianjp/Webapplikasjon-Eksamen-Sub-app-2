0a32bfbd3796572ce77fd152e0592d833be13871
Stianjp <114061998+Stianjp@users.noreply.github.com>
2024-11-26 18:12:41 +0100

    Fikser linker i sidebar

--- sub-app-2/src/Shared/Sidebar.tsx
+++ sub-app-2/src/Shared/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Nav, NavDropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 import LogoGreen from '../icons/LogoGreen.svg'; 
 import './Sidebar.css';
 
@@ -12,19 +13,19 @@ const Sidebar: React.FC = () => {
         <h1 className="h5 mb-0">FoodBank</h1>
       </div>
       <Nav className="flex-column nav-pills mt-3">
-        <Nav.Link href="./Home/Home" className="text-dark d-flex align-items-center p-3">
+        <Nav.Link as={Link} to="/" className="text-dark d-flex align-items-center p-3">
           <i className="bi bi-house-door-fill me-2"></i> Home
         </Nav.Link>
         <NavDropdown title="Products" className="text-dark d-flex align-items-center p-3">
-          <NavDropdown.Item href="./Products/ProductPage">All products</NavDropdown.Item>
+          <NavDropdown.Item as={Link} to="/products">All products</NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item href="./Products/CreateProduct">Add product</NavDropdown.Item>
-          <NavDropdown.Item href="./Products/MyProducts">My products</NavDropdown.Item>
+          <NavDropdown.Item as={Link} to="/products/add">Add product</NavDropdown.Item>
+          <NavDropdown.Item as={Link} to="/products/my">My products</NavDropdown.Item>
         </NavDropdown>
         <NavDropdown title="Account" className="text-dark d-flex align-items-center p-3">
-          <NavDropdown.Item href="./Home/Account">My Account</NavDropdown.Item>
+          <NavDropdown.Item as={Link} to="/account">My Account</NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item href="./Home/Privacy">Privacy</NavDropdown.Item>
+          <NavDropdown.Item as={Link} to="/Privacy">Privacy</NavDropdown.Item>
         </NavDropdown>
       </Nav>
     </div>
