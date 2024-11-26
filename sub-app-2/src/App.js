4e4989f9cabde67c05f1b434d64664a2e58e84af
Stianjp <114061998+Stianjp@users.noreply.github.com>
2024-11-26 18:03:21 +0100

    Legger til riktige elementer i App.js

--- sub-app-2/src/App.js
+++ sub-app-2/src/App.js
@@ -1,8 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './Shared/Sidebar';
 import Home from './Home/Home';
+import ProductPage from './Products/ProductPage';
+import MyProducts from './Products/MyProducts';
+import CreateProduct from './Products/CreateProduct';
+import Account from './Home/Account';
+import Privacy from './Home/Privacy';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './styles/App.css';
+
 
 function App() {
   return (
@@ -12,12 +18,11 @@ function App() {
       <main className="flex-grow-1 p-3">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
+            <Route path="/products" element={<ProductPage />} />
             <Route path="/products/my" element={<MyProducts />} />
             <Route path="/products/add" element={<CreateProduct />} />
             <Route path="/account" element={<Account />} />
             <Route path="/privacy" element={<Privacy />} />
-            <Route path="/login" element={<Login />} />
           </Routes>
         </main>
         </div>
--- sub-app-2/src/Shared/Sidebar.tsx
+++ sub-app-2/src/Shared/Sidebar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Nav, NavDropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import LogoGreen from '../icons/LogoGreen.svg'; // Sørg for at denne banen er riktig
+import LogoGreen from '../icons/LogoGreen.svg'; 
 import './Sidebar.css';
 
 const Sidebar: React.FC = () => {
@@ -12,19 +12,19 @@ const Sidebar: React.FC = () => {
         <h1 className="h5 mb-0">FoodBank</h1>
       </div>
       <Nav className="flex-column nav-pills mt-3">
-        <Nav.Link href="/home" className="text-dark d-flex align-items-center p-3">
+        <Nav.Link href="./Home/Home" className="text-dark d-flex align-items-center p-3">
           <i className="bi bi-house-door-fill me-2"></i> Home
         </Nav.Link>
         <NavDropdown title="Products" className="text-dark d-flex align-items-center p-3">
-          <NavDropdown.Item href="#action/3.1">All products</NavDropdown.Item>
+          <NavDropdown.Item href="./Products/ProductPage">All products</NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.2">Add product</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">My products</NavDropdown.Item>
+          <NavDropdown.Item href="./Products/CreateProduct">Add product</NavDropdown.Item>
+          <NavDropdown.Item href="./Products/MyProducts">My products</NavDropdown.Item>
         </NavDropdown>
         <NavDropdown title="Account" className="text-dark d-flex align-items-center p-3">
-          <NavDropdown.Item href="#action/3.4">My Account</NavDropdown.Item>
+          <NavDropdown.Item href="./Home/Account">My Account</NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.5">Privacy</NavDropdown.Item>
+          <NavDropdown.Item href="./Home/Privacy">Privacy</NavDropdown.Item>
         </NavDropdown>
       </Nav>
     </div>
