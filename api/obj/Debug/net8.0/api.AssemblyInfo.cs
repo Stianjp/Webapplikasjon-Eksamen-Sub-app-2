05b254c62820fe9ab67315b1af2cd43bca171177
NazliQ <nazlipikulser@gmail.com>
2024-11-27 12:51:51 +0100

    Flyttet apiLog til riktig sted

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2e2a35d2f6bc69f8f326dd4c9e26995dc98a4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c007e634b835c0bf7c44b6da902e3952b4fd2c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
