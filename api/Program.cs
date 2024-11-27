53925486121d348171dc1cc5719382983917a2bc
NazliQ <nazlipikulser@gmail.com>
2024-11-27 12:16:19 +0100

    Added a lunch file

--- api/Program.cs
+++ api/Program.cs
@@ -1,49 +1,106 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Sub_App_1.DAL;
 using Serilog;
 using Serilog.Events;
+using Sub_App_1.DAL;
+using Sub_App_1.DAL.Interfaces;
+using Sub_App_1.DAL.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers().AddNewtonsoftJson(options => {
-    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-});
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ItemDbContext>(options => {
-    options.UseSqlite(builder.Configuration["ConnectionStrings:ItemDbContextConnection"]);});
-
-builder.Services.AddControllers();
-builder.Services.AddCors(options =>
-        {
-            options.AddPolicy("CorsPolicy",
-                builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-        });
-
-builder.Services.AddScoped<IItemRepository, ItemRepository>();
-
-var loggerConfiguration = new LoggerConfiguration()
-    .MinimumLevel.Information() // levels: Trace< Information < Warning < Erorr < Fatal
+// Use Serilog for logging
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
     .WriteTo.File($"APILogs/app_{DateTime.Now:yyyyMMdd_HHmmss}.log")
+    .CreateBootstrapLogger()
     .Filter.ByExcluding(e => e.Properties.TryGetValue("SourceContext", out var value) &&
                             e.Level == LogEventLevel.Information &&
                             e.MessageTemplate.Text.Contains("Executed DbCommand"));
-var logger = loggerConfiguration.CreateLogger();
-builder.Logging.AddSerilog(logger);
+    var logger = Log.Logger.CreateLogger();
+    builder.Logging.AddSerilog(logger);
+
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services)
+    .Enrich.FromLogContext()
+    .WriteTo.Console());
+
+// Add services to the container
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options => {
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add ApplicationDbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") + ";Cache=Shared"),
+    ServiceLifetime.Scoped);
+
+// Register Identity services
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequiredUniqueChars = 1;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+// Register repositories
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+// Add CORS policy for API
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin()
+                         .AllowAnyMethod()
+                         .AllowAnyHeader());
+});
+
+// Configure Kestrel
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    options.Configure(context.Configuration.GetSection("Kestrel"));
+});
 
 var app = builder.Build();
 
+// Seed the database
+await DBInit.SeedAsync(app);
+
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    DBInit.Seed(app);
     app.UseSwagger();
     app.UseSwaggerUI();
-    
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseCors("CorsPolicy");
-app.MapControllerRoute(name: "api", pattern: "{controller}/{action=Index}/{id?}");
-    
+app.UseAuthentication();
+app.UseAuthorization();
+
+// API routes
+app.MapControllers();
+
+app.Lifetime.ApplicationStopped.Register(Log.CloseAndFlush);
+
 app.Run();
