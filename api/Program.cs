77d87192d9ed8cbfa1f78c88f10df7b9afb97d01
Magnus <s375129@oslomet.no>
2024-11-28 19:40:09 +0100

    Fix db errors on startup

--- api/Program.cs
+++ api/Program.cs
@@ -90,14 +90,28 @@ builder.WebHost.ConfigureKestrel((context, options) =>
 
 var app = builder.Build();
 
+// Ensure the database is created
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    context.Database.EnsureCreated();
+}
+
+// Seed the database
+await DBInit.SeedAsync(app);
+
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-    await DBInit.SeedAsync(app);
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts(); // Force HTTPS
+}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
