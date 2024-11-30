b1e63081d5f961fc810ed10de22a8eb984aa8478
NazliQ <nazlipikulser@gmail.com>
2024-11-30 11:29:34 +0100

    Updatet privacy to be a card

--- sub-app-2/src/Home/Privacy.jsx
+++ sub-app-2/src/Home/Privacy.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Card } from 'react-bootstrap';
 
 const Privacy = () => {
     return(
         <Container> 
-            <div className="AllProducts">
-                <h1>Privacy Page</h1>
+            <Card>
+                <Card.Body>
+                <Card.Title>Privacy Page</Card.Title>
+                <Card.Text>
                 <p>We do not collect any personal information. This site is used solely for educational purposes, and the only data stored in our databases are your username, password, and the products you add.</p>
                 <h4>Use of data</h4>
                 <p>All data entered on our website is stored in a database to display our product to all users. Since this is solely a university project, the data will only be shared with our workgroup and any university evaluators.</p>
-            </div>
+                </Card.Text>
+            </Card.Body>
+            </Card>
         </Container>
     );
 };
